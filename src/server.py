905d1bb4782d362bb5dd1723638b9d213b1ce439
vidhipandya29 <vidhi.pandya@shopify.com>
2025-03-19 22:23:11 -0400

    added virus on a network

--- src/server.py
+++
@@ -1,7 +0,0 @@
-from model import ContentSpreadModel
-from visualization import create_model_visualization
-
-# Create and launch the server
-server = create_model_visualization(ContentSpreadModel)
-server.port = 8521  # The default
-server.launch() 
